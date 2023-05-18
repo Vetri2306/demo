541ee79bd2c8b551bab982718da806e2b2685bab
Vetri2306 <Vetri2306@users.noreply.github.com>
2023-05-18 14:04:58 +0530

    commit

---
+++ src/main/java/Base/Baseclass.java
@@ -0,0 +1,14 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Baseclass {
+
+}
---
+++ src/main/java/Baseactions/Mouseactions.java
@@ -0,0 +1,5 @@
+package Baseactions;
+
+public class Mouseactions {
+
+}
